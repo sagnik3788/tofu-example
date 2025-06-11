8879112ffe5d67b938d624973ccdb1e76d9fa0c2
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2025-06-12 00:11:23 +0530

    Update main.tf

--- terraform-dev/main.tf
+++ terraform-dev/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = "tofu-example-bucket-111111111111111"
+  bucket = "tofu-bucket-test"
 
   tags = {
     Name        = "Example bucket"
