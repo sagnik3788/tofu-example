4d56192c9ae2a90ff4ce23d6f3242092189e7b80
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2025-07-23 12:17:27 +0530

    Update main.tf

--- terraform-dev/main.tf
+++ terraform-dev/main.tf
@@ -1,14 +1,13 @@
-# main.tf
 provider "aws" {
-  region = var.aws_region      
+  region = "us-east-1"
 }
 
 resource "aws_s3_bucket" "example" {
-  bucket = var.bucket_name     
+  bucket = "tofu-plugin-test-128346262737272"
 
   tags = {
     Name        = "Example bucket"
-    Environment = var.environment  
+    Environment = "dev"
   }
 }
 
