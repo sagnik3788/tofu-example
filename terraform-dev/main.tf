d400fe2cb29af33d23b00fbc14bc92952680a501
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2025-07-23 12:20:54 +0530

    Update main.tf

--- terraform-dev/main.tf
+++ terraform-dev/main.tf
@@ -1,27 +1,22 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
-resource "aws_s3_bucket" "example" {
-  bucket = "tofu-plugin-test-128346262737272"
-
-  tags = {
-    Name        = "Example bucket"
-    Environment = "dev"
+terraform {
+  required_providers {
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.5"
+    }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
   }
 }
 
-resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = aws_s3_bucket.example.id
-
-  rule {
-    object_ownership = "BucketOwnerPreferred"
-  }
+resource "random_pet" "name" {
+  length = 2
 }
 
-resource "aws_s3_bucket_acl" "example" {
-  depends_on = [aws_s3_bucket_ownership_controls.example]
-
-  bucket = aws_s3_bucket.example.id
-  acl    = "private"
+resource "null_resource" "demo" {
+  triggers = {
+    generated_name = random_pet.name.id
+  }
 }
