42e198d2db62fa06b2ff568e9c333ce09d8ce391
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2025-08-02 01:01:01 +0530

    Update main.tf

--- terraform-dev/main.tf
+++ terraform-dev/main.tf
@@ -12,25 +12,11 @@ terraform {
 }
 
 resource "random_pet" "name" {
-  length = 2
-}
-
-resource "random_integer" "rand" {
-  min = 1
-  max = 100
+  length = 4
 }
 
 resource "null_resource" "demo" {
   triggers = {
     generated_name = random_pet.name.id
-    random_number  = random_integer.rand.result
   }
-
-  provisioner "local-exec" {
-    command = "echo Generated pet name: ${random_pet.name.id} with number ${random_integer.rand.result}"
-  }
-}
-
-output "final_output" {
-  value = "Pet: ${random_pet.name.id}, Number: ${random_integer.rand.result}"
 }
