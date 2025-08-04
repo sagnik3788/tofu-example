ba187c0e10710b8aa0574737ae5a0c26704d537a
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2025-08-05 01:36:48 +0530

    Update main.tf

--- terraform-dev/main.tf
+++ terraform-dev/main.tf
@@ -11,19 +11,19 @@ terraform {
   }
 }
 
-# Module 1 - Simple local module
+# Module 1 - Inline module definition
 module "local_module" {
   source = "./modules/local"
   version = "v1.0.0"
 }
 
-# Module 2 - Another local module
+# Module 2 - Inline module definition
 module "test_module" {
   source = "./modules/test"
   version = "v2.0.0"
 }
 
-# Module 3 - Third local module
+# Module 3 - Inline module definition
 module "demo_module" {
   source = "./modules/demo"
   version = "v3.0.0"
@@ -37,5 +37,21 @@ resource "random_pet" "name" {
 resource "null_resource" "demo" {
   triggers = {
     generated_name = random_pet.name.id
+  }
+}
+
+# Create the module directories and files automatically
+resource "null_resource" "create_modules" {
+  triggers = {
+    timestamp = timestamp()
+  }
+
+  provisioner "local-exec" {
+    command = <<-EOT
+      mkdir -p modules/local modules/test modules/demo
+      echo 'resource "random_pet" "local_pet" { length = 2 }' > modules/local/main.tf
+      echo 'resource "random_string" "test_string" { length = 8 }' > modules/test/main.tf
+      echo 'resource "null_resource" "demo_resource" { triggers = { timestamp = timestamp() } }' > modules/demo/main.tf
+    EOT
   }
 }
