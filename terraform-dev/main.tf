c23410232fef013db1d383599c136ef48cde0eba
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2025-08-05 01:34:12 +0530

    Update main.tf

--- terraform-dev/main.tf
+++ terraform-dev/main.tf
@@ -4,26 +4,38 @@ terraform {
       source  = "hashicorp/random"
       version = "~> 3.5"
     }
+    null = {
+      source  = "hashicorp/null"
+      version = "~> 3.0"
+    }
   }
 }
 
-# Simple modules for testing DetermineVersions
-module "test_module_1" {
-  source  = "hashicorp/random"
-  version = "3.5.0"
+# Module 1 - Simple local module
+module "local_module" {
+  source = "./modules/local"
+  version = "v1.0.0"
 }
 
-module "test_module_2" {
-  source  = "hashicorp/random"
-  version = "3.6.0"
+# Module 2 - Another local module
+module "test_module" {
+  source = "./modules/test"
+  version = "v2.0.0"
 }
 
-module "test_module_3" {
-  source  = "hashicorp/random"
-  version = "3.7.0"
+# Module 3 - Third local module
+module "demo_module" {
+  source = "./modules/demo"
+  version = "v3.0.0"
 }
 
-# Simple resource
+# Your existing resources
 resource "random_pet" "name" {
   length = 4
+}
+
+resource "null_resource" "demo" {
+  triggers = {
+    generated_name = random_pet.name.id
+  }
 }
