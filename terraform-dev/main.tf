dc3043575b54993b90c90ed4431d7db94503a23c
Sagnik Das <116512372+sagnik3788@users.noreply.github.com>
2025-08-05 01:40:56 +0530

    Update main.tf

--- terraform-dev/main.tf
+++ terraform-dev/main.tf
@@ -11,22 +11,20 @@ terraform {
   }
 }
 
-# Module 1 - Inline module definition
-module "local_module" {
-  source = "./modules/local"
-  version = "v1.0.0"
+# Registry modules with versions
+module "random_pet_module" {
+  source  = "hashicorp/random"
+  version = "3.5.0"
 }
 
-# Module 2 - Inline module definition
-module "test_module" {
-  source = "./modules/test"
-  version = "v2.0.0"
+module "random_string_module" {
+  source  = "hashicorp/random"
+  version = "3.6.0"
 }
 
-# Module 3 - Inline module definition
-module "demo_module" {
-  source = "./modules/demo"
-  version = "v3.0.0"
+module "null_resource_module" {
+  source  = "hashicorp/null"
+  version = "3.2.0"
 }
 
 # Your existing resources
@@ -37,21 +35,5 @@ resource "random_pet" "name" {
 resource "null_resource" "demo" {
   triggers = {
     generated_name = random_pet.name.id
-  }
-}
-
-# Create the module directories and files automatically
-resource "null_resource" "create_modules" {
-  triggers = {
-    timestamp = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = <<-EOT
-      mkdir -p modules/local modules/test modules/demo
-      echo 'resource "random_pet" "local_pet" { length = 2 }' > modules/local/main.tf
-      echo 'resource "random_string" "test_string" { length = 8 }' > modules/test/main.tf
-      echo 'resource "null_resource" "demo_resource" { triggers = { timestamp = timestamp() } }' > modules/demo/main.tf
-    EOT
   }
 }
